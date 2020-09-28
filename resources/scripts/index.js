881ccd890dc788291582116e33b7dc803fa1a76d
Xiao Xiao <49316508+Xiao0909@users.noreply.github.com>
2020-09-27 23:58:14 -0400

    new change

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -4,13 +4,14 @@ const display = document.querySelector('.display');
 const myBtn = document.querySelector('.myBtn');
 const stored = localStorage.getItem('textinput')
 
-if(storage){
-    display.textContent = stored
-}
 
 storage.addEventListener('input', letter =>{
     display.textContent = letter.target.value
 })
+
+if(storage){
+    display.textContent = stored
+}
 
 const saveToLocalStorage = () =>{
     localStorage.setItem('textinput', display.textContent)
